6a18a96e7b79c5d0a281d33767d1f392dc54f33a
smhabib99-99 <habibaust34@gmail.com>
2023-10-25 18:57:44 +0600

    update product server side is successful

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ async function run() {
     app.get('/coffee/:id', async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query);
+      res.send(result);
     })
 
     // for post=create data to database
